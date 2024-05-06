ee501b5ec3d29759ccdb4a9b944dd5716a3f2483
zion4d <zion4d@gmail.com>
2024-05-06 12:32:07 +0300

    release

--- app.py
+++ app.py
@@ -5,7 +5,3 @@ if __name__ == "__main__":
     book = load_data()
     mainmenu(book)
     save_data(book)
-
-
-# book.add_entry(Entry("2024-01-01", True, 1000, "1st"))
-# book.add_entry(Entry("2024-02-02", False, 500, "2nd"))
